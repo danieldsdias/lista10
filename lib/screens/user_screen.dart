84cffcd7173f6369a3e9d1d63e5b4230a70e4676
Daniel Dias <ddias@stone.com.br>
2022-06-18 18:27:02 -0300

    Image and Camera picker

--- lib/screens/user_screen.dart
+++ lib/screens/user_screen.dart
@@ -1,27 +1,94 @@
+import 'dart:io';
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:lista10/widgets/image_widget.dart';
 
-class UserScreen extends StatelessWidget {
+class UserScreen extends StatefulWidget {
   const UserScreen({Key? key}) : super(key: key);
 
   static const routeName = '/user';
+
+  @override
+  State<UserScreen> createState() => _UserScreenState();
+}
+
+class _UserScreenState extends State<UserScreen> {
+  File? image;
 
   void _signOut(BuildContext context) {
     FirebaseAuth.instance.signOut();
     Navigator.pop(context);
   }
 
+  Future pickImage(ImageSource source) async {
+    try {
+      final image = await ImagePicker().pickImage(source: source);
+
+      if (image == null) return;
+
+      final imageTemp = File(image.path);
+      setState(() => this.image = imageTemp);
+    } on PlatformException catch (e) {
+      print('Failed to pick image: $e');
+    }
+  }
+
+  Widget buildButton({
+    required String title,
+    required IconData icon,
+    required VoidCallback onClicked,
+  }) =>
+      ElevatedButton(
+          onPressed: onClicked,
+          child: Row(
+            children: [
+              Icon(icon, size: 28),
+              const SizedBox(width: 16),
+              Text(title),
+            ],
+          ));
+
   @override
   Widget build(BuildContext context) {
     final user = FirebaseAuth.instance.currentUser!;
+    const double imageSize = 160;
 
     return Scaffold(
       appBar: AppBar(title: const Text('Usuário')),
-      body: Padding(
+      body: Container(
         padding: const EdgeInsets.all(32),
         child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            //Spacer(),
+            ClipOval(
+              child: image != null
+                  ? ImageWidget(
+                      image: image!,
+                      onClicked: (source) => pickImage(source),
+                      size: imageSize)
+                  : const FlutterLogo(size: imageSize),
+            ),
+            const SizedBox(height: 24),
+            image == null
+                ? Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      buildButton(
+                          title: 'Galeria',
+                          icon: Icons.image_outlined,
+                          onClicked: () => pickImage(ImageSource.gallery)),
+                      const Padding(
+                          padding: EdgeInsets.only(left: 20, right: 20)),
+                      buildButton(
+                          title: 'Camera',
+                          icon: Icons.camera_alt_outlined,
+                          onClicked: () => pickImage(ImageSource.camera)),
+                    ],
+                  )
+                : const SizedBox(height: 24),
             const Text(
               'Conectado como',
               style: TextStyle(fontSize: 16),
